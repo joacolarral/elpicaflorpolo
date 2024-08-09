c6b6bd8882627c915526f7afa1beacbe0a5ee774
jlarralde <Joaquin.Larralde@ice.com>
2024-08-09 15:00:37 -0300

    configurando dns

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,22 @@ import withVideos from "next-videos";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
+  basePath: "",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 // Combinamos los plugins
 const configWithPlugins = withNextIntl({ ...nextConfig, ...withVideos() });
