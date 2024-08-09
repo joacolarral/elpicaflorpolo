5d41367429a3011149dbfcc12b6796476aee91c3
jlarralde <Joaquin.Larralde@ice.com>
2024-08-09 18:52:22 -0300

    all card linked and with cursor pointed

--- src/app/containers/Home/CalendarHome/CalendarCards.tsx
+++ src/app/containers/Home/CalendarHome/CalendarCards.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useTranslations } from "next-intl";
 import React, { Dispatch, SetStateAction } from "react";
 
 import Button from "@/app/components/Button";
-import { SEASON } from "@/app/constants";
+import { NAVBAR_ITEMS, SEASON } from "@/app/constants";
 import styles from "@/app/containers/Home/home.module.scss";
 import Autumn from "@/app/images/home/Temporada_otoño.png";
 import AutumnHover from "@/app/images/home/Temporada_otoño_hover.png";
@@ -24,9 +25,24 @@ const CalendarCards: React.FC<CalendarCardsProps> = ({
 }) => {
   const calendarHomeT = useTranslations("HOME.CALENDAR_SECTION");
   const cards = [
-    { season: SEASON.AUTUMN, defaultImage: Autumn, hoverImage: AutumnHover },
-    { season: SEASON.SPRING, defaultImage: Spring, hoverImage: SpringHover },
-    { season: SEASON.SUMMER, defaultImage: Summer, hoverImage: SummerHover },
+    {
+      season: SEASON.AUTUMN,
+      defaultImage: Autumn,
+      hoverImage: AutumnHover,
+      linkTo: NAVBAR_ITEMS.PICAFLOR,
+    },
+    {
+      season: SEASON.SPRING,
+      defaultImage: Spring,
+      hoverImage: SpringHover,
+      linkTo: NAVBAR_ITEMS.PICAFLOR,
+    },
+    {
+      season: SEASON.SUMMER,
+      defaultImage: Summer,
+      hoverImage: SummerHover,
+      linkTo: NAVBAR_ITEMS.PATAGONIA,
+    },
   ];
 
   const isHovered = (seasonCard: SeasonType) => seasonCard === hoveredCard;
@@ -34,21 +50,22 @@ const CalendarCards: React.FC<CalendarCardsProps> = ({
   return (
     <div className={styles.calendarCardsContainer}>
       {cards.map((card) => (
-        <div
-          key={card.season}
-          className={styles.calendarCard}
-          onMouseEnter={() => setHoveredCard(card.season)}
-        >
-          <Image
-            width={248}
-            height={400}
-            src={isHovered(card.season) ? card.hoverImage : card.defaultImage}
-            alt={card.season}
-          />
-          <Button disabled={!isHovered(card.season)} isSecondaryButton>
-            {calendarHomeT("BUTTON")}
-          </Button>
-        </div>
+        <Link key={card.season} href={`#${card.linkTo}`}>
+          <div
+            className={styles.calendarCard}
+            onMouseEnter={() => setHoveredCard(card.season)}
+          >
+            <Image
+              width={248}
+              height={400}
+              src={isHovered(card.season) ? card.hoverImage : card.defaultImage}
+              alt={card.season}
+            />
+            <Button disabled={!isHovered(card.season)} isSecondaryButton>
+              {calendarHomeT("BUTTON")}
+            </Button>
+          </div>
+        </Link>
       ))}
     </div>
   );
