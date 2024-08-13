095c3092a4b12d6d236b2470fbd5c928dff2b4d0
jlarralde <Joaquin.Larralde@ice.com>
2024-08-13 11:54:53 -0300

    added cards and fix carousel cards

--- src/app/containers/Home/CalendarHome/CalendarCards.tsx
+++ src/app/containers/Home/CalendarHome/CalendarCards.tsx
@@ -2,20 +2,33 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useTranslations } from "next-intl";
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 
 import Button from "@/app/components/Button";
 import Carousel from "@/app/components/Carousel";
 import { NAVBAR_ITEMS, SEASON } from "@/app/constants";
 import styles from "@/app/containers/Home/home.module.scss";
 import useIsMobile from "@/app/hooks/useIsMobile";
-import Autumn from "@/app/images/home/Temporada_otoño.png";
-import AutumnHover from "@/app/images/home/Temporada_otoño_hover.png";
-import Spring from "@/app/images/home/Temporada_primavera.png";
-import SpringHover from "@/app/images/home/Temporada_primavera_hover.png";
-import Summer from "@/app/images/home/Temporada_verano.png";
-import SummerHover from "@/app/images/home/Temporada_verano_hover.png";
+import Autumn from "@/app/images/home/season_autumn.png";
+import AutumnHover from "@/app/images/home/season_autumn_hover.png";
+import Spring from "@/app/images/home/season_spring.png";
+import SpringHover from "@/app/images/home/season_spring_hover.png";
+import SummerHover from "@/app/images/home/season_summer_hover.png";
+import Summer from "@/app/images/home/season_verano.png";
+import Otoño from "@/app/images/home/Temporada_otoño.png";
+import OtoñoHover from "@/app/images/home/Temporada_otoño_hover.png";
+import Primavera from "@/app/images/home/Temporada_primavera.png";
+import PrimaveraHover from "@/app/images/home/Temporada_primavera_hover.png";
+import Verano from "@/app/images/home/Temporada_verano.png";
+import VeranoHover from "@/app/images/home/Temporada_verano_hover.png";
 import { SeasonType } from "@/app/types";
 
 interface CalendarCardsProps {
@@ -34,24 +47,32 @@ const CalendarCards: React.FC<CalendarCardsProps> = ({
 }) => {
   const [isMounted, setIsMounted] = useState(false);
   const isMobile = useIsMobile();
+  const pathname = usePathname();
   const calendarHomeT = useTranslations("HOME.CALENDAR_SECTION");
+
+  const currentLang = useMemo(() => {
+    if (pathname.includes("/es")) return "es";
+    if (pathname.includes("/en")) return "en";
+    return "";
+  }, [pathname]);
+
   const cards = [
     {
       season: SEASON.AUTUMN,
-      defaultImage: Autumn,
-      hoverImage: AutumnHover,
+      defaultImage: currentLang === "en" ? Autumn : Otoño,
+      hoverImage: currentLang === "en" ? AutumnHover : OtoñoHover,
       linkTo: NAVBAR_ITEMS.PICAFLOR,
     },
     {
       season: SEASON.SPRING,
-      defaultImage: Spring,
-      hoverImage: SpringHover,
+      defaultImage: currentLang === "en" ? Spring : Primavera,
+      hoverImage: currentLang === "en" ? SpringHover : PrimaveraHover,
       linkTo: NAVBAR_ITEMS.PICAFLOR,
     },
     {
       season: SEASON.SUMMER,
-      defaultImage: Summer,
-      hoverImage: SummerHover,
+      defaultImage: currentLang === "en" ? Summer : Verano,
+      hoverImage: currentLang === "en" ? SummerHover : VeranoHover,
       linkTo: NAVBAR_ITEMS.PATAGONIA,
     },
   ];
@@ -67,7 +88,7 @@ const CalendarCards: React.FC<CalendarCardsProps> = ({
     value: (
       <Link href={`#${card.linkTo}`}>
         <div className={styles.calendarCard}>
-          <Image width={265} src={card.hoverImage} alt={card.season} />
+          <Image src={card.hoverImage} alt={card.season} />
           <Button isSecondaryButton>{calendarHomeT("BUTTON")}</Button>
         </div>
       </Link>
